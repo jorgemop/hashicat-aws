9380ccb8c77219e7cce3b58a1e35ca2be8556efb
jorgemop <ovalles.jorge@gmail.com>
2023-05-02 19:36:56 +0200

    error3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,10 +2,9 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "my-s3-bucket-sdf"
-  acl    = "private"
 
   versioning = {
-    enabled = true
+    enabled = false
   }
 
 }
